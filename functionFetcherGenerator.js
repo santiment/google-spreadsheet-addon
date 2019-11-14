838962032b3de92e2ea35474b092051b90a8b24b
spiderjako <lyudmil_danailov@abv.bg>
2019-11-14 16:21:07 +0200

    Add function fetcher as a sheet function

---
+++ functionFetcherGenerator.js
@@ -0,0 +1,34 @@
+const jsdoc2md = require('jsdoc-to-markdown')
+const inputFiles = ['./src/sheetFunctions.js']
+
+function generate () {
+  let listOfFunctions = []
+  for (let element of functions_()) {
+    listOfFunctions.push(`    '${element}'`)
+  }
+  let generatedListOfFunctions = `[\n` + listOfFunctions.join(`,\n`) + `\n  ]`
+  let functionString =
+`/**
+ * Returns all of the available functions, which SanSheets has to offer.
+ * @returns {Array} all functions in the addon.
+ * @customfunction
+ */
+function SAN_FUNCTIONS () {
+  var listOfFunctions = ${generatedListOfFunctions}
+  return handleErrors_(listOfFunctions)
+}
+`
+  return functionString
+}
+
+function functions_ () {
+  let doc = jsdoc2md.getTemplateDataSync({ files: inputFiles })
+    .sort(function (a, b) { return a.name.localeCompare(b.name) })
+    .map(x => x.name)
+
+  return doc
+}
+
+module.exports = {
+  generate: generate
+}
--- src/generatedSheetFunctions.js
+++ src/generatedSheetFunctions.js
@@ -270,3 +270,44 @@ function SAN_NVT_RATIO (projectSlug, from, to) {
   return handleErrors_(getMetric_)('nvt', projectSlug, from, to, {  })
 }
 
+/**
+ * Returns all of the available functions, which SanSheets has to offer.
+ * @returns {Array} all functions in the addon.
+ * @customfunction
+ */
+function SAN_FUNCTIONS () {
+  var listOfFunctions = [
+    'SAN_ACTIVE_DEPOSITS',
+    'SAN_ALL_PROJECTS',
+    'SAN_DAILY_CLOSING_PRICE',
+    'SAN_DEV_ACTIVITY',
+    'SAN_EMERGING_TRENDS',
+    'SAN_ERC20_PROJECTS',
+    'SAN_ETH_SPENT_OVER_TIME',
+    'SAN_ETH_TOP_TRANSACTIONS',
+    'SAN_EXCHANGE_FUNDS_FLOW',
+    'SAN_GAS_USED',
+    'SAN_GITHUB_ACTIVITY',
+    'SAN_HISTORICAL_BALANCE',
+    'SAN_HISTORY_TWITTER_DATA',
+    'SAN_LATEST_PRICE',
+    'SAN_MINERS_BALANCE',
+    'SAN_MINING_POOLS_DISTRIBUTION',
+    'SAN_NETWORK_GROWTH',
+    'SAN_NEWS',
+    'SAN_OHLC',
+    'SAN_PRICE_ABSOLUTE_CHANGE',
+    'SAN_PRICE_PERCENT_CHANGE',
+    'SAN_PRICE_VOLUME_DIFF',
+    'SAN_PRICES',
+    'SAN_PROJECT_FUNDAMENTALS',
+    'SAN_PROJECT_SOCIAL_DATA',
+    'SAN_SOCIAL_DOMINANCE',
+    'SAN_SOCIAL_VOLUME',
+    'SAN_SOCIAL_VOLUME_PROJECTS',
+    'SAN_TOKEN_AGE_CONSUMED',
+    'SAN_TOKEN_TOP_TRANSACTIONS',
+    'SAN_TOP_HOLDERS_PERCENT_OF_TOTAL_SUPPLY'
+  ]
+  return handleErrors_(listOfFunctions)
+}
