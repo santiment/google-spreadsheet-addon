f5599c1e05ee481fe7689819f44bc609d39450bd
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2020-01-23 17:02:08 +0200

    Remove warning from SAN_OHLC

--- src/functions/priceFunctions.js
+++ src/functions/priceFunctions.js
@@ -81,8 +81,6 @@ function ohlc_ (slug, from, to) {
   var results = getApiClient_().fetchOhlc(slug, from, to)
   assertHasData_(results)
 
-  var warning = ['WARNING! The order has changed from close->high->low->open to open->high->low->close']
-
   var headers = [
     'Date',
     'Open Price USD',
@@ -91,7 +89,7 @@ function ohlc_ (slug, from, to) {
     'Close Price USD'
   ]
 
-  return [warning].concat([headers].concat(results.map(function (result) {
+  return [headers].concat(results.map(function (result) {
     return [
       formatDatetimeField_(result.datetime),
       formatNumber_(result.openPriceUsd),
@@ -99,7 +97,7 @@ function ohlc_ (slug, from, to) {
       formatNumber_(result.lowPriceUsd),
       formatNumber_(result.closePriceUsd)
     ]
-  })))
+  }))
 }
 
 function priceVolumeDiff_ (currency, slug, from, to) {
