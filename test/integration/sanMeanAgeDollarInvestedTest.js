6c953101734e87c29a2a834111ed2bcdef6c27e2
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2020-02-14 18:04:37 +0200

    Add tests for mean dollar invested age

---
+++ test/integration/sanMeanAgeDollarInvestedTest.js
@@ -0,0 +1,36 @@
+const { testFieldTypes } = require('../helper.js')
+
+const { testHandlesNullData, assertNumberOfRecords, assertDaysMatch } = require('../integration_helper.js')
+
+const { slug, from, to, numberOfDays, days } = require('../setup.js')
+
+describe('SAN_MEAN_DOLLAR_INVESTED_AGE', () => {
+  const expected = {
+    date: 'string',
+    value: 'number'
+  }
+
+  const response = san.SAN_MEAN_AGE(slug, from, to)
+  const headers = response[0]
+  const results = response[1]
+  testFieldTypes(results, expected)
+  testHandlesNullData(
+    'fetchGetMetric',
+    san.SAN_MEAN_DOLLAR_INVESTED_AGE,
+    slug,
+    from,
+    to)
+
+  it('has proper headers', () => {
+    const expectedHeaders = ['Date', 'Value']
+    expect(headers).to.deep.equal(expectedHeaders)
+  })
+
+  it('returns a record per every day', () => {
+    const results = san.SAN_MEAN_AGE(slug, from, to)
+
+    assertNumberOfRecords(results, numberOfDays)
+
+    assertDaysMatch(results, days)
+  })
+})
