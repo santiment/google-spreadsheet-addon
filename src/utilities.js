1dced2b4be24e8b43ec48b3cf60a16899fdc4c71
spiderjako <lyudmil_danailov@abv.bg>
2020-11-09 12:46:53 +0200

    Fixing issue when the date is a DATE, not a string

--- src/utilities.js
+++ src/utilities.js
@@ -29,22 +29,25 @@ function formatNumber_ (field) {
 
 function roundToMinutes_ (date) {
   const ms = 1000 * 60 * 5
-  const roundedDate = new Date(Math.round(date.getTime() / ms) * ms)
+  const roundedDate = new Date(Math.floor(date.getTime() / ms) * ms)
 
   return roundedDate
 }
 
 function toUTC_ (date) {
   const timezone = SpreadsheetApp.getActive().getSpreadsheetTimeZone()
-  if (date.startsWith('utc_now')) {
-    const dt = new Date()
-    const [firstPart, secondPart] = date.split('-')
-    if ((typeof secondPart) !== 'undefined') {
-      dt.setDate(dt.getDate() - Number(secondPart.substring(0, secondPart.length - 1)))
-    }
+  if (typeof date === 'string') {
+    if (date.startsWith('utc_now')) {
+      const dt = new Date()
+      const [firstPart, secondPart] = date.split('-')
+      if ((typeof secondPart) !== 'undefined') {
+        dt.setDate(dt.getDate() - Number(secondPart.substring(0, secondPart.length - 1)))
+      }
 
-    return Utilities.formatDate(roundToMinutes_(dt), timezone, "yyyy-MM-dd'T'HH:mm:ss'Z'")
+      return Utilities.formatDate(roundToMinutes_(dt), timezone, "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    }
   }
+
   return Utilities.formatDate(roundToMinutes_(new Date(date)), timezone, "yyyy-MM-dd'T'HH:mm:ss'Z'")
 }
 
