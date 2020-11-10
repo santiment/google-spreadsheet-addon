59d66c58312a23bda7854e2b10bfb333c2993855
spiderjako <lyudmil_danailov@abv.bg>
2020-11-10 22:31:49 +0200

    Removing code 200 errors from the caching

--- src/api/connection.js
+++ src/api/connection.js
@@ -60,7 +60,7 @@ Connection_.prototype.fetchQuery = function (query) {
     code: response.getResponseCode(),
     body: JSON.parse(response.getContentText())
   }
-  if (response.getResponseCode() === 200) {
+  if (response.getResponseCode() === 200 && response.errors === null) {
     cache.put(key, JSON.stringify(returnedResponse), 21600)
   }
 
