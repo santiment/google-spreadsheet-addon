6f419f8abc8c540971125b8a8638382d95331666
spiderjako <lyudmil_danailov@abv.bg>
2021-02-23 15:19:30 +0200

    Add information about reddit source

--- test/integration/sanSocialVolumeTest.js
+++ test/integration/sanSocialVolumeTest.js
@@ -1,23 +1,49 @@
-require('../support/helper.js')
 
-const { testHandlesNullData } = require('../support/integrationHelper.js')
-const { slug, from, to } = require('../support/setup.js')
+const { testFieldTypes } = require('../support/helper.js')
+const { testHandlesNullData, assertNumberOfRecords, assertDaysMatch } = require('../support/integrationHelper.js')
+const { slug, from, to, numberOfDays, days } = require('../support/setup.js')
 
 describe('SAN_SOCIAL_VOLUME', () => {
-  const socialVolumeType = 'TELEGRAM_CHATS_OVERVIEW'
+  const expected = {
+    date: 'string',
+    value: 'number'
+  }
 
-  testHandlesNullData(
-    'fetchSocialVolume',
-    san.SAN_SOCIAL_VOLUME,
-    slug,
-    from,
-    to,
-    socialVolumeType)
+  const response = san.SAN_SOCIAL_VOLUME(slug, from, to)
+  const headers = response[0]
+  const results = response[1]
+
+  testFieldTypes(results, expected)
+  testHandlesNullData('fetchGetMetric', san.SAN_SOCIAL_VOLUME, slug, from, to)
 
   it('has proper headers', () => {
-    const response = san.SAN_SOCIAL_VOLUME(slug, from, to, socialVolumeType)
-    const headers = response[0]
+    const expectedHeaders = ['Date', 'Value']
+    expect(headers).to.deep.equal(expectedHeaders)
+  })
 
-    expect(headers).to.deep.equal(['Date', 'Value'])
+  it('returns a record per every day', () => {
+    const results = san.SAN_SOCIAL_VOLUME(slug, from, to)
+
+    assertNumberOfRecords(results, numberOfDays)
+
+    assertDaysMatch(results, days)
+  })
+
+  it('returns a record per every day, per every source', () => {
+    const sources = [
+      'TELEGRAM_DISCUSSION_OVERVIEW',
+      'TELEGRAM_CHATS_OVERVIEW',
+      'PROFESSIONAL_TRADERS_CHAT_OVERVIEW',
+      'DISCORD_DISCUSSION_OVERVIEW',
+      'REDDIT_COMMENTS_OVERVIEW',
+      'TWITTER_CHATS_OVERVIEW'
+    ]
+    sources.forEach(
+      source => {
+        const results = san.SAN_SOCIAL_VOLUME(slug, from, to, source)
+        assertNumberOfRecords(results, numberOfDays)
+        assertDaysMatch(results, days)
+      }
+    )
   })
 })
