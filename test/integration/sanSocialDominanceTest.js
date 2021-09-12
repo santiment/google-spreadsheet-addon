918c30c07b40be7e5354d72edbcb171bf07b0672
Lyudmil Danailov <spiderjako@Lyudmils-MacBook-Pro.local>
2021-09-12 20:46:33 +0300

    Fixing tests

--- test/integration/sanSocialDominanceTest.js
+++ test/integration/sanSocialDominanceTest.js
@@ -1,36 +1,60 @@
-
 const { testFieldTypes } = require('../support/helper.js')
-const { testHandlesNullData, assertNumberOfRecords, assertDaysMatch } = require('../support/integrationHelper.js')
-const { slug, from, to, numberOfDays, days, numberOfHours } = require('../support/setup.js')
+
+const {
+  testHandlesNullData,
+  assertNumberOfRecords
+} = require('../support/integrationHelper.js')
+
+const {
+  slug,
+  from,
+  to,
+  days,
+  numberOfDays,
+  formatDate
+} = require('../support/setup.js')
 
 describe('SAN_SOCIAL_DOMINANCE', () => {
   const expected = {
     date: 'string',
-    value: 'number'
+    dominance: 'number'
   }
 
-  const response = san.SAN_SOCIAL_DOMINANCE(slug, from, to)
+  const source = 'ALL'
+  const response = san.SAN_SOCIAL_DOMINANCE(slug, from, to, source)
   const headers = response[0]
   const results = response[1]
 
   testFieldTypes(results, expected)
-  testHandlesNullData('fetchGetMetric', san.SAN_SOCIAL_DOMINANCE, slug, from, to)
+
+  testHandlesNullData(
+    'fetchGetMetric',
+    san.SAN_SOCIAL_DOMINANCE,
+    slug,
+    from,
+    to,
+    source)
 
   it('has proper headers', () => {
     const expectedHeaders = ['Date', 'Value']
     expect(headers).to.deep.equal(expectedHeaders)
   })
 
-  it('returns a record per every day', () => {
-    const results = san.SAN_SOCIAL_DOMINANCE(slug, from, to)
-
+  it('returns a record per every day according to the given source', () => {
+    const results = san.SAN_SOCIAL_DOMINANCE(slug, from, to, source)
     assertNumberOfRecords(results, numberOfDays)
 
-    assertDaysMatch(results, days)
+    for (let [index, day] of days.entries()) {
+      expect(results[index + 1][0]).to.equal(formatDate(day))
+    }
   })
 
-  it('returns a record per hour', () => {
-    const addresses = san.SAN_SOCIAL_DOMINANCE(slug, from, to, '1h')
-    assertNumberOfRecords(addresses, numberOfHours)
+  it('returns a record per every day according to the default source', () => {
+    const results = san.SAN_SOCIAL_DOMINANCE(slug, from, to)
+    assertNumberOfRecords(results, numberOfDays)
+
+    for (let [index, day] of days.entries()) {
+      expect(results[index + 1][0]).to.equal(formatDate(day))
+    }
   })
 })
