f82aeb2c9f1df61cd2f2b5b96510e128ce2ddca2
Lyudmil Danailov <spiderjako@Lyudmils-MacBook-Pro.local>
2023-12-05 13:09:09 +0200

    Adding RSI functions to SanSheets

---
+++ test/integration/sanRsi1dTest.js
@@ -0,0 +1,30 @@
+const { testFieldTypes } = require('../support/helper.js')
+const { testHandlesNullData, assertNumberOfRecords, assertDaysMatch } = require('../support/integrationHelper.js')
+const { slug, from, to, numberOfDays, days } = require('../support/setup.js')
+
+describe('SAN_RSI_1D', () => {
+  const expected = {
+    date: 'string',
+    value: 'number'
+  }
+
+  const response = san.SAN_RSI_1D(slug, from, to)
+  const headers = response[0]
+  const results = response[1]
+
+  testFieldTypes(results, expected)
+  testHandlesNullData('fetchGetMetric', san.SAN_RSI_1D, slug, from, to)
+
+  it('has proper headers', () => {
+    const expectedHeaders = ['Date', 'Value']
+    expect(headers).to.deep.equal(expectedHeaders)
+  })
+
+  it('returns a record per every day', () => {
+    const results = san.SAN_RSI_1D(slug, from, to)
+
+    assertNumberOfRecords(results, numberOfDays)
+
+    assertDaysMatch(results, days)
+  })
+})
